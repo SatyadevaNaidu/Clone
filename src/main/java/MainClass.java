e0c181306eabb8c198aea48f5c65af7384bdbeec
satya <satyadevanaidu1306@gmail.com>
2021-03-14 23:28:01 +0100

    cal

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,19 +1,45 @@
-public class MainClass {
-    int A;
-}
-class Sample{
 
-}
+import java.util.Scanner;
 
-class Satya{
+class Main {
 
-}
-class Aravind{
+    public static void main(String[] args) {
 
-}
-class Koushik {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
 
-}
-class Ness {
+        // nextDouble() reads the next double from the keyboard
+        double first = reader.nextDouble();
+        double second = reader.nextDouble();
 
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = reader.next().charAt(0);
+
+        double result = 0.0;
+
+        switch (operator) {
+            case '+':
+                result = first + second;
+                break;
+
+            case '-':
+                result = first - second;
+                break;
+
+            case '*':
+                result = first * second;
+                break;
+
+            case '/':
+                result = first / second;
+                break;
+
+            // operator doesn't match any case constant (+, -, *, /)
+            default:
+                System.out.printf("Error! operator is not correct");
+                return;
+        }
+
+        System.out.println(first + " " + operator + " " + second + " = " + result);
+    }
 }
